912f7c4ee895c3c5180598d0aeb041897126a7c7
Linho17 <kyrylinho@gmail.com>
2021-06-16 22:14:10 +0300

    Add files via upload

---
+++ SettingsKey.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsKey 
+{
+    const string MUTE_KEY = "isMute";
+    const string DISTANCE = "distance";
+    const string FISH = "fish";
+    const string DISTANCE_ALL_TIME = "distanceAllTime";
+    const string DISTANCE_HIGHSCORE = "distanceHighscore";
+    const string FISH_COLLECT = "fishCollect";
+
+
+    const bool MUTE_DEFAULT = false;
+    public static void SetMute(bool mute)
+    {
+        if (mute)
+            PlayerPrefs.SetString(MUTE_KEY, "true");
+        else
+            PlayerPrefs.SetString(MUTE_KEY, "false");
+
+       
+    }
+
+    public static bool GetMute()
+    {
+        string temp = PlayerPrefs.GetString(MUTE_KEY);
+        if (temp == "true")
+            return true;
+        else
+            return false;
+    }
+
+
+
+    public static void SetDistance(int _distance)
+    {
+        PlayerPrefs.SetInt(DISTANCE, _distance);
+    }
+
+    public static void SetFish(int _fish)
+    {
+        PlayerPrefs.SetInt(FISH, _fish);
+    }
+
+    public static void SetDistanceAndFish(int _distance, int _fish)
+    {
+        SetFish(_fish);
+        SetDistance(_distance);
+        PlayerPrefs.Save();
+    }
+
+    public static void SetFishCollect(int _fish)
+    {
+        PlayerPrefs.SetInt(FISH_COLLECT, GetFishCollect() + _fish);
+    }
+
+    public static int GetDistance()
+    {
+       return PlayerPrefs.GetInt(DISTANCE);
+    }
+
+    public static int GetFish()
+    {
+        return PlayerPrefs.GetInt(FISH);
+    }
+    public static int GetFishCollect()
+    {
+        return PlayerPrefs.GetInt(FISH_COLLECT);
+    }
+
+
+    public static int GetDistanceAllTime()
+    {
+        return PlayerPrefs.GetInt(DISTANCE_ALL_TIME);
+    }
+
+    public static int GetDistanceHighscore()
+    {
+        return PlayerPrefs.GetInt(DISTANCE_HIGHSCORE);
+    }
+
+
+    public static void SetDistanceHighscore(int _distance)
+    {
+         PlayerPrefs.SetInt(DISTANCE_HIGHSCORE, _distance);
+    }
+
+
+    public static void SetDistanceAllTime(int _distance)
+    {
+        PlayerPrefs.SetInt(DISTANCE_ALL_TIME, GetDistanceAllTime()+_distance);
+    }
+
+
+
+
+}
---
+++ Swipe.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum StatusSwipe
+{
+    None,
+    SliceUP,
+    SliceDown,
+    HoldUp,
+    HoldDown
+};
+
+public class Swipe : MonoBehaviour
+{
+
+    [SerializeField] private float minSwipeDistance;
+    [SerializeField] private float MaxSwipeDuration;
+
+    private float timeSwipeStart;
+    private float timeSwipeEnd;
+    private float swipeTime;
+    
+
+    private Vector2 startSwipePosition;
+    private Vector2 endSwipePosition;
+    private float swipeLength;
+
+    
+    public StatusSwipe TouchReader()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                timeSwipeStart = Time.time;
+                startSwipePosition = touch.position;
+            }
+
+            else if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
+            {
+                
+                endSwipePosition = touch.position;
+                swipeLength = (endSwipePosition - startSwipePosition).magnitude;
+                
+                if (swipeLength > minSwipeDistance)
+                {
+                    if ((endSwipePosition - startSwipePosition).y > 0)
+                    {
+                        return StatusSwipe.HoldUp;
+                    }
+                    else
+                    {
+                        return StatusSwipe.HoldDown;
+                    }
+                    
+                }
+
+            }
+
+            else if(touch.phase == TouchPhase.Ended)
+            {
+
+                timeSwipeEnd = Time.time;
+                swipeTime = timeSwipeEnd - timeSwipeStart;
+
+                endSwipePosition = touch.position;
+                swipeLength = (endSwipePosition - startSwipePosition).magnitude;
+
+                if (swipeTime < MaxSwipeDuration && swipeLength > minSwipeDistance)
+                {
+                    if ((endSwipePosition - startSwipePosition).y > 0)
+                    {
+                        return StatusSwipe.SliceUP;
+                    }
+                    else
+                    {
+                        return StatusSwipe.SliceDown;
+                    }
+                }
+            }
+        }
+
+        return StatusSwipe.None;
+
+    }
+}
